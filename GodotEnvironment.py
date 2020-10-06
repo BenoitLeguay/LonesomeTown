70c1e8193a6bd15f77ba7d873adcb97dc211e8cf
LeVaillaud <hugovaillaud@gmail.com>
2020-10-06 02:28:49 +0200

    the test godot environment is now fully working, with render choice for every episode and accelerated FPS

--- GodotEnvironment.py
+++ GodotEnvironment.py
@@ -12,6 +12,10 @@ class GodotEnvironment:
         self.socket = None
         self.client_socket = None
 
+        self.godot_process = None
+        self.is_godot_launched = False
+        self.is_rendering = True
+
         self.set_params_from_dict(params)
 
 
@@ -19,51 +23,91 @@ class GodotEnvironment:
         self.host = params.get("host", '127.0.0.1')
         self.port = params.get("port", 4242)
 
+
+    # Connection functions =============================================================================================
+
+
     def initialize_socket(self):
+        """
+        Creates a socket
+        :return:
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def end_connection(self):
+        """Closes the socket, and then reset it. Also reset client socket."""
         self.socket.close()
+        self.socket = None
+        self.client_socket = None
 
     def wait_for_connection(self):
+        """"""
         self.socket.bind((self.host, self.port))
         self.socket.listen()
         self.client_socket, addr = self.socket.accept()
         print('Connected by', addr)
 
-    def create_request(self, initialization=False, action=None):
+    def create_request(self, initialization=False, termination=False, action=None):
         request = {}
         request["initialization"] = initialization
-        if isinstance(action, np.ndarray):
-            action = action.tolist()
-        request["action"] = action
+        request["termination"] = termination
+        request["render"] = self.is_rendering
+        if initialization == False and termination == False:
+            if isinstance(action, np.ndarray):
+                action = action.tolist()
+            request["action"] = int(action)
         request = json.dumps(request).encode()
+        #print(request)
         return request
 
     def get_environment_state(self):
         condition = False
         data_in = None
         while condition != True:
-            data_in = self.client_socket.recv(80)
-            # print(data_in)
+            data_in = self.client_socket.recv(10000)
             data_in = data_in.decode()
             if len(data_in) > 4:
                 condition = True
-        # print(data_in)
+
         data_in = json.loads(data_in)  # TODO: change the format of the data, and therefore change that too
+        #print(data_in)
         return data_in
 
-    def reset(self):
+
+    # main functions ===================================================================================================
+
+
+    def reset(self, render):
         """
         Gets the initial state of the environment
         :return: the first state of the environment
         """
-        self.initialize_socket()
-        subprocess.Popen("{} --main-pack {}".format(
-            os.path.join("c:/", "Users", "Hugo", "Desktop", "Godot_v3.2.3-stable_win64.exe"),
-            os.path.join("c:/", "Users", "Hugo", "Documents", "dev", "godot_projects", "test_tcp",
-                         "Godot-Test-v0.pck")), shell=True)
-        print("coucou")
+
+        if (render != self.is_rendering) and self.is_godot_launched:
+            if self.socket is None:
+                self.initialize_socket()
+            self.wait_for_connection()
+
+            termination_request = self.create_request(termination=True)
+            self.client_socket.sendall(termination_request)
+            self.end_connection()
+            self.is_godot_launched = False
+            self.is_rendering = render
+
+
+        if self.socket is None:
+            self.initialize_socket()
+
+        if self.is_godot_launched == False:
+            godot_path = os.path.join("c:/", "Users", "Hugo", "Desktop", "Godot_v3.2.3-stable_win64.exe")
+            environment_path = os.path.join("c:/", "Users", "Hugo", "Documents", "AI", "projects", "LonesomeTown",
+                                            "godot_environments", "test_tcp", "Godot-Test-v0.pck")
+            command = "{} --main-pack {}".format(godot_path, environment_path)
+            if not self.is_rendering:
+                command = command + " --disable-render-loop --no-window"
+            self.godot_process = subprocess.Popen(command, shell=True)
+            self.is_godot_launched = True
+
         self.wait_for_connection()
         # send the first request
         first_request = self.create_request(initialization=True)
@@ -86,7 +130,6 @@ class GodotEnvironment:
         self.client_socket.sendall(request)
 
         data_in = self.get_environment_state()
-        #print(data_in)
         state = data_in["state"]
         state[0] /= 250
         state[1] /= 150
@@ -94,10 +137,22 @@ class GodotEnvironment:
         done = data_in["done"]
         n_frames = data_in["n_frames"]
         reward = reward/n_frames
-
         if done == True:
             self.end_connection()
         return state, reward, done, n_frames
 
     def render(self):
-        pass
+        if not self.is_rendering:
+            self.is_rendering = True
+            if self.is_godot_launched:
+                #self.godot_process.kill()
+                self.create_request(termination=True)
+                self.is_godot_launched = False
+
+    def dont_render(self):
+        if self.is_rendering:
+            self.is_rendering = False
+            if self.is_godot_launched:
+                self.create_request(termination=True)
+                self.is_godot_launched = False
+
